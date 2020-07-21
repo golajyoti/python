b9f81df5690cba65b2cf6aee7ec20bc879e50e64
golajyoti <68595807+golajyoti@users.noreply.github.com>
2020-07-21 18:53:20 +0530

    Add files via upload

---
+++ area of rectangle.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jul 11 16:35:01 2020
+
+@author: JYOTI
+"""
+Length = float(input("Enter the Length of a rectangle :"))
+Breadth = float(input("Enter the breadth of a rectangle :"))
+area = Length*Breadth
+print("the area of rectangle having Length" , Length, "and Breadth", Breadth, "is :", area)
+
