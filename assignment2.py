16af3e90a2e764fe8c992a351507fdf77d7748a7
golajyoti <68595807+golajyoti@users.noreply.github.com>
2020-07-21 20:16:05 +0530

    Add files via upload

---
+++ assignment2.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jul 21 18:58:02 2020
+
+@author: JYOTI
+"""
+
+
+a= 15
+b= 13.5
+c= a+b
+d= a-b
+e= a/b
+f= a*b
+g= a//b
+print((("sum of", a, "and", b, "is", c,
+        "subtraction of", a , "and",b,"is",d,
+        "division of", a , "and",b,"is",e,
+        "multiplication of", a , "and",b,"is",f,
+        "floor of", a , "and",b,"is",g)))
