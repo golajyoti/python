1b0b22b11753a44ac84be7589a206a3fbf240d1b
golajyoti <68595807+golajyoti@users.noreply.github.com>
2020-07-23 23:18:07 +0530

    Add files via upload

---
+++ sum of n numbers.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jul 23 22:27:43 2020
+
+@author: JYOTI
+"""
+
+
+num = int(input("Enter the number :"))
+sum=0
+num1= num
+while(num1>=0):
+    sum=sum+num1
+    num1=num1-1
+print("The sum of numbers from 1 to", num, "is", sum)
