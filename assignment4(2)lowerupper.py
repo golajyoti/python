22b575a3e652bb65b0ea748eee098c916f228e3d
golajyoti <68595807+golajyoti@users.noreply.github.com>
2020-07-27 10:51:01 +0530

    Add files via upload

---
+++ assignment4(2)lowerupper.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jul 27 10:43:17 2020
+
+@author: JYOTI
+"""
+
+str1 = str(input("Enter the input:"))
+a = str1.islower()
+print(" Is the given string is in lower case:",a)
+b = str1.isupper()
+print(" Is the given string is in Upper case:",b)
